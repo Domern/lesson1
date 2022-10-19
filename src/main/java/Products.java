72265bf14fdf70d58de3013ac00133f260ebdaca
Ярослав <zzzqqqzzzwww@yandex.ru>
2022-10-19 22:36:39 +0300

    Initial commit

---
+++ src/main/java/Products.java
@@ -0,0 +1,11 @@
+public class Products {
+    private int id;
+    private String title;
+    private int coast;
+
+    public Products(int id, String title, int coast) {
+        this.id = id;
+        this.title = title;
+        this.coast = coast;
+    }
+}
